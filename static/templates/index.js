f769f285308f38ee4bf912ad5d81c33e39b05d44
Paul Frazee <pfrazee@gmail.com>
2020-04-16 10:43:48 -0500

    Prompt non-beaker visitors to download beaker

--- static/templates/index.js
+++ static/templates/index.js
@@ -1,14 +1,23 @@
-document.querySelector('.create-drive').addEventListener('click', async (e) => {
-  var drive = await beaker.hyperdrive.createDrive()
-  for (let path of TEMPLATE_FILES) {
-    let v = await fetch(TEMPLATE_ROOT + path).then(res => res.text())
-    if (path.startsWith('/ui/')) {
-      // HACK
-      // hugo doesnt serve the . folders so we have to fudge it for /.ui/ folders
-      // -prf
-      path = '/.ui/' + path.slice('/ui/'.length)
+var createDriveButton = document.querySelector('.create-drive')
+if (typeof beaker !== 'undefined' && typeof beaker.hyperdrive !== 'undefined') {
+  createDriveButton.textContent = 'Create Drive from this Template'
+  createDriveButton.addEventListener('click', async (e) => {
+    var drive = await beaker.hyperdrive.createDrive()
+    for (let path of TEMPLATE_FILES) {
+      let v = await fetch(TEMPLATE_ROOT + path).then(res => res.text())
+      if (path.startsWith('/ui/')) {
+        // HACK
+        // hugo doesnt serve the . folders so we have to fudge it for /.ui/ folders
+        // -prf
+        path = '/.ui/' + path.slice('/ui/'.length)
+      }
+      await drive.writeFile(path, v)
     }
-    await drive.writeFile(path, v)
-  }
-  window.open(drive.url)
-})
+    window.open(drive.url)
+  })
+} else {
+  createDriveButton.textContent = 'Get Beaker v0.9+ to Create This Site'
+  createDriveButton.addEventListener('click', e => {
+    window.open('https://beakerbrowser.com/')
+  })
+}
